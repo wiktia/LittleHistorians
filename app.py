43bb3df08245928cd58d75bd45dc71ce7b8c9c0c
Piotr Sikora <piotrsikora@student.agh.edu.pl>
2025-06-13 15:08:38 +0200

    zmianastylu, kodu, stron aby działały z flaskiem

--- app.py
+++ app.py
@@ -10,4 +10,26 @@ def logowanie():
 def start_quiz():
     name = request.form.get("name")
     avatar = request.form.get("avatar")
-    return render_template("test.html", name=name, avatar=avatar)
+    return render_template("test2.html", name=name, avatar=avatar)
+
+@app.route("/startscreen")
+def startscreen():
+    return render_template("startscreen.html")
+
+@app.route('/puzzle')
+def puzzle():
+    return render_template("puzzle.html")
+@app.route("/endscreen")
+def endscreen():
+    return render_template("endscreen.html")
+
+@app.route("/text_to_image")
+def text_to_image():
+    return render_template("text_to_image.html")
+
+@app.route("/timeline")
+def timeline():
+    return render_template("timeline.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
